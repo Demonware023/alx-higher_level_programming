f5a20408807e6b833b9d4de0d8a67cdb9eb4203b
Demonware023 <awamchimere@icloud.com>
2024-01-29 12:39:40 -0800

    Change representation

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Creates a module that computes the area, perimeter, magic methods (__str__, __repr__, __del__) and changes reprsentation"""
+
+
+class Rectangle:
+    """ A class that gets the area(...) and changes representation"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height) if self.__width != 0 and self.__height != 0 else 0
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
