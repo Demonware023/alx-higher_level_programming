6ffb27fc73b5e8202d0d54685829fbfff86e1f03
Demonware023 <awamchimere@icloud.com>
2024-02-11 12:36:25 -0800

    Rectangle instance to dictionary representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,75 +14,26 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif (value <= 0):
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif (value <= 0):
-            raise ValueError("height must be > 0")
-        else:
-            self.__height = value
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        elif (value < 0):
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
-
-    @property
-    def y(self):
-        return (self.__y)
-
-    @y.setter
-    def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        elif (value < 0):
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
-
-    def area(self):
-        return self.width * self.height
-
-    def display(self):
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
-
-    def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle."""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
 
     def update(self, *args, **kwargs):
         if args:
             attrs = ['id', 'width', 'height', 'x', 'y']
-            for i, arg in enumerate(args):
-                setattr(self, attrs[i], arg)
+            for i, attr in enumerate(args):
+                setattr(self, attrs[i], attr)
         else:
             for key, value in kwargs.items():
-                setattr(self, key, value)
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -56,5 +56,35 @@ class TestSquare(unittest.TestCase):
                 s1.size = "xyz"
             self.assertTrue("width must be an intger" in str(context.exception))
 
+        def test_update_with_args(self):
+            s1 = Square(5)
+            s1.update(10)
+            self.assertEqual(str(s1), "[Square] (10) 0/0 - 5")
+
+            s1.update(1, 2)
+            self.assertEqual(str(s1), "[Square] (1) (0/0 - 2")
+
+            s1.update(1, 2, 3)
+            self.assertEqual(str(s1), "[Square] (1) 3/0 - 2")
+
+	    s1.update(1, 2, 3, 4)
+            self.assertEqual(str(s1), "[Square] (1) 3/4 - 2")
+
+        def test_update_with_kwargs(self):
+            s1 = Square(5)
+            s1.update(x=12)
+            self.assertEqual(str(s1), "[Square] (1) 12/0 - 5")
+
+	    s1.update(size=7, y=1)
+            self.assertEqual(str(s1), "[Square] (1) 12/1 - 7")
+
+            s1.update(size=7, id=89, y=1)
+            self.assertEqual(str(s1), "[Square] (89) 12/1 - 7")
+
+        def test_update_mixed(self):
+            s1 = Square(5)
+            s1.update(10, 2, y=3)
+            self.assertEqual(str(s1), "[Square] (10) 0/3 - 2")
+
 if __name__ == '__main__':
     unittest.main()
