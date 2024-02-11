4f9991a775aed80a71fb0f093b3dfe477af29715
Demonware023 <awamchimere@icloud.com>
2024-02-11 12:13:28 -0800

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,18 +14,23 @@ class Square(Rectangle):
         Rectangle class.
     """
     def __init__(self, size, x=0, y=0,id=None):
+        """ Initializes a Square instance."""
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        """Getter method for size attribute."""
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        """Setter method for size attribute."""
-        self.width = value
-        self.height = value
+    def update(self, *args, **kwargs):
+        """Updates attributes of the Square instance."""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
         """ Returns the spring reprsentaion of the square."""
