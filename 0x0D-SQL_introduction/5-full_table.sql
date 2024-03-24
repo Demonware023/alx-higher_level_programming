ac4e340682ca40cdf0e4a32715dde079dbbcd5d7
Demonware023 <awamchimere@icloud.com>
2024-03-23 21:49:58 -0700

    Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server. The database name will be passed as an argument of the mysql command. You are not allowed to use the DESCRIBE or EXPLAIN statements
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-  AND TABLE_NAME = 'first_table';
+-- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server. The database name will be passed as an argument of the mysql command. You are not allowed to use the DESCRIBE or EXPLAIN statement.
+SHOW CREATE TABLE 'first_table';
