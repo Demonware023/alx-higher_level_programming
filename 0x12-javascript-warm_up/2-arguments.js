93a399639947d54d9502c46871322bcec77c89ae
Demonware023 <awamchimere@icloud.com>
2024-04-08 16:51:34 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,12 @@
 /*
    Write a script that prints a message depending of the number of argument
    passed; If no arguments are passed to the script, print “No argument" If
-   only one argument is passed to the script, print “Argument found” 
+   only one argument is passed to the script, print “Argument found”
    Otherwise, print “Arguments found”
 */
 
-//process.argv is an array containing the command-line arguments passed.
-argsCount = process.argv.length - 2;
+// process.argv is an array containing the command-line arguments passed.
+const argsCount = process.argv.length - 2;
 
 if (argsCount === 0) {
   console.log('No argument');
