8009dd13c4ff8396ed3ef64245bf19937fb5f668
Demonware023 <awamchimere@icloud.com>
2024-04-08 16:23:14 -0700

    First constant, first print

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// A script that prints "JavaScript is amazing".
+
+const myVar = 'JavaScript is amazing' // Stores the string in a variable
+console.log(myVar) // Prints the output
