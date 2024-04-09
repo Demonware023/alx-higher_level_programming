b3e944d3d05aec718534a91295e95fea36cc39c4
Demonware023 <awamchimere@icloud.com>
2024-04-08 18:03:19 -0700

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 /*
    Write a script that prints a square:
    The first argument is the size of the square
-   If the first argument can’t be converted to an integer, print 
+   If the first argument can’t be converted to an integer, print
    “Missing size”You must use the character X to print the square.
 */
 
