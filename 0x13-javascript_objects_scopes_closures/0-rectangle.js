4ddd84c1e08183b4511c6cc024c0802af01f32c9
Demonware023 <awamchimere@icloud.com>
2024-04-11 14:51:27 -0700

    Rectangle #0

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Creates an empty class Rectangle using the class notation.
+
+class Rectangle {
+  constructor() {
+    /*
+       constructor is empty so when an instance of a rectangle is created
+       it will have an empty object representation.
+    */
+  }
+}
