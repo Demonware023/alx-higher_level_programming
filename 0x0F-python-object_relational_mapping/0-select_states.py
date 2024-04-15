e91dc7a47078f17de949cd7ace5e167c124ba0fe
Demonware023 <awamchimere@icloud.com>
2024-04-15 13:40:13 -0700

    Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get MySQL username, password, and database name from command-line arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server running on localhost at port 3306
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+
+        cursor = db.cursor()
+
+        # Execute SELECT query to retrieve states sorted by id
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Fetch all rows
+        rows = cursor.fetchall()
+
+        # Display results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    finally:
+        if 'db' in locals():
+            db.close()
+
