80bfcd96fcf691ff533bc90e6e7e4bc8b925799b
Demonware023 <awamchimere@icloud.com>
2024-04-25 14:46:53 -0700

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of the response if the status code is 200
-curl -sL -w "%{http_code}" "$1" | grep -q 200 && curl -sL "$1"
+curl -sL -w "%{http_code}" -o /dev/null | grep -q "200" && curl -s "$1"
