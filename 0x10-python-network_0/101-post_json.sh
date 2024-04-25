1aef8151f8401033eee13b7060e9cfa87b8b78af
Demonware023 <awamchimere@icloud.com>
2024-04-25 16:05:28 -0700

    cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 # Send a JSON POST request with the contents of the file as the body
-curl -sX POST -H "Content-type: application/json" -d @"$2" "$1"
+curl -s "$1" -X POST -H "Content-type: application/json" -d @"$2"
