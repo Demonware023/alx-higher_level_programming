2f7d2aec62be2a8000d21c734eef3d84c51df5c4
Demonware023 <awamchimere@icloud.com>
2024-04-26 13:04:36 -0700

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -16,21 +16,15 @@ import requests
 import sys
 
 # Extract username and personal access token from command-line arguments
-username = sys.argv[1]
-password = sys.argv[2]
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
 
-# Construct the URL for the GitHub API endpoint to get user information
-url = f"https://api.github.com/user"
+    # Send a GET request to the GitHub API endpoint with Basic Authentication
+    response = requests.get('https://api.github.com/user', auth=(username, token))
 
-# Send a GET request to the GitHub API endpoint with Basic Authentication
-response = requests.get(url, auth=(username, password))
-
-# Check if the request was successful and the response contains
-# -user information
-if response.status_code == 200:
-    # Parse the JSON response
-    user_info = response.json()
-    # Display the user ID
-    print(user_info['id'])
-else:
-    print("None")
+    # Check if the request was successful and the response contains user information
+    if response.status_code >= 400:
+        print("None")
+    else:
+        print(response.json().get('id'))
