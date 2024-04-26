83b5333c7547c0d6d17c0291bc8f5b11d2acb162
Demonware023 <awamchimere@icloud.com>
2024-04-26 12:01:33 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -18,17 +18,18 @@ import urllib.error
 # within this block will be executed. If the script is being imported
 # as a module in another script, this code block will not be executed.
 if __name__ == "__main__":
+    # Extract the URL from command-line arguments
     url = sys.argv[1]
-    # sys.argv[1] is the first command-line argument. In this case,
-    # it’s the URL that you want to send a request to.
+
     try:
-        with urllib.request.urlopen(url) as response:
-	    # The urlopen function from the urllib.request module is used to open the URL
-	    # The read method is used to read the response, and decode is
-	    #used to decode the response from bytes to a string using utf-8 encoding.
-	    print(response.read().decode('utf-8'))
-    except urllib.error.HTTPError as error:
-        # This is the except block that catches HTTPError exceptions
-        # which are raised for non-200 HTTP codes. If an HTTPError is
-        # caught, the script prints "Error code: " followed by the HTTP status code.
-        print("Error code: {}".format(error.code))
+        # Send a GET request to the URL
+        with urlopen(url) as response:
+            # Read and decode the body of the response
+            decoded_content = response.read().decode("utf-8")
+        
+            # Display the body of the response
+            print(decoded_content)
+
+    except HTTPError as e:
+        # Print the error code if an HTTPError occurs
+        print("Error code:", e.code)
