ae545cfd4ff42b133a475edacc609e0a3163e791
Demonware023 <awamchimere@icloud.com>
2024-04-26 12:30:36 -0700

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,8 @@ Write a Python script that fetches https://alx-intranet.hbtn.io/status
 
 You must use the package requests
 You are not allow to import packages other than requests
-The body of the response must be display like the following example (tabulation before -)
+The body of the response must be display like the following example
+-(tabulation before -)
 """
 import requests
 
