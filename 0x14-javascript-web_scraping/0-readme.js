5a67dba426ba1f9cc82790178cb357bf4b2114a2
Demonware023 <awamchimere@icloud.com>
2024-05-22 05:42:02 -0700

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+
+// The first argument is the file path
+// The content of the file must be read in utf-8
+// If an error occurred during the reading, print the error object
+// console err/err prints the error object
+
+const fs = require('fs');
+const filePath = process.argv[2]; // The first argument is the file path
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        // If an error occurred during the reading, print the error object
+        console.error(err);
+    } else {
+        // If the file is read successfully, print its content
+        console.log(data);
+    }
+});
